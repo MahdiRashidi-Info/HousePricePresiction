19266c0e9e9a39aaa5afd7a215a9e7190a8ca14c
mahdi rashidi <rashidi.mahdi2001@gmail.com>
2023-12-22 01:33:55 +0330

    ready to develope

--- MabaheshVizheProject/house_price_assistance/static/scripts.js
+++ MabaheshVizheProject/house_price_assistance/static/scripts.js
@@ -1,20 +1,5 @@
-var delayInMilliseconds = 500;
-
-setTimeout(function () {
-  var dropdown = document.getElementById("dropdown");
-  dropdown.addEventListener("change", function () {
-    var selectedIndex = dropdown.selectedIndex;
-    console.log("Selected index:", selectedIndex);
-  });
-}, delayInMilliseconds);
-
-// const inputs = document.querySelectorAll('input[type="radio"]');
-// inputs.forEach((input) => {
-//   input.addEventListener("change", () => {
-//     inputs.forEach((otherInput) => {
-//       if (otherInput !== input) {
-//         otherInput.removeAttribute("checked");
-//       }
-//     });
-//   });
-// });
+var dropdown = document.getElementById("dropdown");
+dropdown.addEventListener("change", function () {
+  var selectedIndex = dropdown.selectedIndex;
+  console.log("Selected index:", selectedIndex);
+});
--- MabaheshVizheProject/house_price_assistance/views.py
+++ MabaheshVizheProject/house_price_assistance/views.py
@@ -11,16 +11,20 @@ def examine_view(request):
 
     df = pd.read_csv(file_path)      
     original_addresses = df['Original_Address'].tolist()
-    return render(request, 'user_create.html' ,{'city' : original_addresses})
+    return render(request, 'main_form.html' ,{'city' : original_addresses})
 
 
 def form_submit_view(request):
+
     if request.method == 'POST':
-        form_data = request.POST
+        print(request.POST)
         
-        # print(form_data["nickName"])
+        state = request.POST.get('state')
+        # print(state)
         
         
         return HttpResponse('Your data has been sent')
     else:
         return HttpResponse('Not allowed to call!')
+    
+    # return render(request, 'result.html')
