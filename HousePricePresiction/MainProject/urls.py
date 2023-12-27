4f4e15e6041d80a7f04f4ddd09d08dd875d16fb0
mahdi rashidi <rashidi.mahdi2001@gmail.com>
2023-12-27 20:41:22 +0330

    refactor project name and classes

--- HousePricePresiction/MainProject/urls.py
+++ HousePricePresiction/MainProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for MabaheshVizheProject project.
+URL configuration for MainProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
